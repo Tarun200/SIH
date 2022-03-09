96c17837a822369e9fa014e3e795a5dc71ab193e
Tarun200 <sampathkumartarun@gmail.com>
2022-03-09 17:03:53 +0530

    My commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const multer=require('multer');
+const express=require('express');
+const path=require('path');
+
+
+
+
+
+var storage = multer.diskStorage(
+    {
+        destination: './uploads/',
+        filename: function ( req, file, cb ) {
+            //req.body is empty...
+            //How could I get the new_file_name property sent from client here?
+            cb( null, file.originalname);
+        }
+    }
+);
+
+
+
+var upload=multer({storage:storage});
+
+
+const app=express();
+
+// app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/uploadFile.html'));
+})
+
+app.post('/upload',upload.single('file'));
+
+app.listen(3000);
